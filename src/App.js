ae9c4c4f66f86612ed3ed388acc535453e5b6a93
vedantpatel1997 <vedantp9@gmail.com>
2024-11-24 23:25:09 -0700

    add logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vedant Patel 1.6
+          Vedant Patel 1.7
         </p>
         
       </header>
