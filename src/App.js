8e057b06e36b90c076669c57441efcde81b6490b
vedantpatel1997 <vedantp9@gmail.com>
2025-02-03 11:44:46 -0700

    1.8

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vedant Patel 1.7
+          Vedant Patel 1.8
         </p>
         
       </header>
